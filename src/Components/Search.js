187268b8134666ce8d6154201e7c0a77235f578d
Samantha Jeet <samanthajeet@gmail.com>
2019-06-13 14:11:55 -0700

    attempting to search

---
+++ src/Components/Search.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import axios from 'axios'
+
+class Search extends Component {
+  state = { 
+    userSearchInput: '',
+    searchResults: []
+   }
+
+   handleChange(prop, val){
+     this.setState({ [prop] : val})
+   }
+
+   handleSearch = async() => {
+    let searchTerm = this.state.userSearchInput
+    let response = await axios.get(`https://en.wikipedia.org/w/api.php?action=query&origin=*&format=json&generator=search&gsrnamespace=0&gsrlimit=5&gsrsearch=${searchTerm}`)
+    console.log(response.data.query.pages)
+    this.setState({
+      searchResults: response.data.query.pages
+    })
+   }
+
+  render() { 
+    // let  {searchResults } = this.state
+    // let mappedResults = searchResults.map( (result,index) => {
+    //   return (
+    //     <div key={index} >
+    //       <h2>{result.title}</h2>
+    //     </div>
+    //   )
+    // })
+    return (
+      <>
+        <h2>search here yo</h2>
+        <input onChange={(e) => this.handleChange('userSearchInput', e.target.value)} type="text"/>
+        <button onClick={() => this.handleSearch()} >search</button>
+        {/* {mappedResults} */}
+      </>
+     );
+  }
+}
+ 
+export default Search;
